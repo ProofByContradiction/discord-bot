f0bbeff8ade3eaffa6a2bc073b5a8cf11e3f5f1a
ProofByContradiction <Mckeema@gmail.com>
2022-05-31 09:44:44 -0400

    Add event handler and serenty package.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use serenity::{
+    prelude::*,
+    model::prelude::*,
+    Client,
+};
+
+struct Handler;
+
+impl EventHandler for Handler{
+    fn message(&self, context: Context, msg: Message) {
+        unimplemented!();
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
